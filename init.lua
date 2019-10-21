fb3096c47ecd928373d387ecb4c59fd2bbaa8204
Thomas Rudin <thomas@rudin.io>
2019-10-21 21:39:56 +0200

    sounds

---
+++ init.lua
@@ -0,0 +1,27 @@
+
+soundblock.register({
+  filename = "damocles_soundblock_ambient_1",
+  key = "damocles_soundblock_ambient_1",
+  name = "Ambient background 1"
+})
+
+soundblock.register({
+  filename = "damocles_soundblock_ambient_2",
+  key = "damocles_soundblock_ambient_2",
+  name = "Ambient background 2"
+})
+
+soundblock.register({
+  filename = "damocles_soundblock_spooky_1",
+  key = "damocles_soundblock_spooky_1",
+  name = "Spooky background 1"
+})
+
+soundblock.register({
+  filename = "damocles_soundblock_success_1",
+  key = "damocles_soundblock_success_1",
+  name = "Success 1"
+})
+
+
+print("[OK] soundblock_damocles")
---
+++ readme.md
@@ -0,0 +1,14 @@
+
+# License
+
+* damocles_soundblock_ambient_1.ogg
+  * CC0 https://freesound.org/people/JeltsinSH/sounds/481121/
+
+* damocles_soundblock_ambient_2.ogg
+  * CC0 https://freesound.org/people/Valo/sounds/398340/
+
+* damocles_soundblock_spooky_1.ogg
+  * CC0 https://freesound.org/people/TheDweebMan/sounds/277192/
+
+* damocles_soundblock_success_1.ogg
+  * CC0 https://freesound.org/people/Leszek_Szary/sounds/171670/
